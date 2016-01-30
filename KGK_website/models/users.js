350b79aaca2a621ba601595baae711ec7eae7d60
Andrew Carton <andrew.carton1@gmail.com>
2016-01-30 14:35:14 -0500

    users table migrations

---
+++ KGK_website/models/users.js
@@ -0,0 +1,17 @@
+"use strict";
+module.exports = function(sequelize, DataTypes) {
+  var users = sequelize.define("users", {
+    first_name: DataTypes.STRING,
+    last_name: DataTypes.STRING,
+    email_address: DataTypes.STRING,
+    phone_number: DataTypes.STRING,
+    password_digest: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return users;
+};
